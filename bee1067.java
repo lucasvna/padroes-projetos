15ed9bb6bddf8fd2b10cc292326b88f662050bb5
Lucas Viana de Abrantes <lucasvna@academicos.com>
2024-09-02 20:16:17 -0300

    problemas beecrowd

---
+++ bee1067.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import java.util.Scanner;
+public class bee1067 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int valorX = leitor.nextInt();
+        for (int i = 0; i <= valorX; i++) {
+            if(i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
